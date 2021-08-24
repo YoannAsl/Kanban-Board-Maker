f44737ecd3bb0ba4734b695a055294ce60f1c58d
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-08-24 22:00:31 +0200

    Switched to not using Redux

--- src/features/list/List.tsx
+++ src/features/list/List.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { useAppDispatch } from '../../hooks';
 import Card, { CardProps } from './card/Card';
 
-interface ListProps {
+export interface ListProps {
+	id: number;
 	title: string;
 	cards: CardProps[];
+	addCard: (listId: number) => void;
 }
 
-const List = ({ title, cards }: ListProps) => {
+const List = ({ id, title, cards, addCard }: ListProps) => {
+	// const List = ({ list }: ListProps) => {
+	// const { title, cards } = list;
 	// const dispatch = useAppDispatch();
+
 	return (
 		<li>
 			<h1>{title}</h1>
@@ -21,7 +26,7 @@ const List = ({ title, cards }: ListProps) => {
 					/>
 				))}
 			</ul>
-			{/* <button onClick={() => dispatch()}>Create a new list</button> */}
+			<button onClick={() => addCard(id)}>Create a new card</button>
 		</li>
 	);
 };
--- src/features/list/ListsContainer.tsx
+++ src/features/list/ListsContainer.tsx
@@ -1,21 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../../hooks';
 import { addList } from './listsSlice';
 
-import List from './List';
+// import { CardProps } from './card/Card';
+import List, { ListProps } from './List';
+
+// type ListType = {
+// 	id: number;
+// 	title: string;
+// 	cards: CardProps[];
+// };
 
 const ListsContainer = () => {
-	const dispatch = useAppDispatch();
-	const lists = useAppSelector((state) => state.lists);
+	// const dispatch = useAppDispatch();
+	// const lists = useAppSelector((state) => state.lists);
+	const [lists, setLists] = useState([
+		{
+			id: 0,
+			title: 'Series',
+			cards: [{ id: 0, title: 'SNK', description: 'SNK description' }],
+		},
+		{
+			id: 1,
+			title: 'Movies',
+			cards: [
+				{ id: 0, title: 'Suicide Squad', description: 'meh' },
+				{ id: 1, title: 'Joker', description: 'good' },
+			],
+		},
+	]);
+
+	const addList = () => {
+		const newList = {
+			id: lists.length + 1,
+			title: 'Placeholder',
+			cards: [],
+		};
+		setLists([...lists, newList]);
+	};
+
+	const addCard = (listId: number) => {
+		const list = lists.find((list) => list.id === listId);
+		console.log(list);
+
+		const newCard = {
+			// id: list.cards.length + 1,
+			id: 10,
+			title: 'New card title placeholder',
+			description: 'new card description placholder',
+		};
+		const updatedList = lists[lists.indexOf(list!)].cards.push(newCard);
+		console.log(updatedList);
+		// setLists([...lists]);
+		// console.log(list!.cards);
+	};
 
 	return (
 		<section>
 			{lists.map((list, idx) => (
-				<List key={idx} title={list.title} cards={list.cards} />
+				<List
+					key={idx}
+					id={list.id}
+					title={list.title}
+					cards={list.cards}
+					addCard={addCard}
+				/>
+				// <List key={idx} list={list} />
 			))}
-			<button onClick={() => dispatch(addList())}>
-				Create a new list
-			</button>
+			{/* <button onClick={() => dispatch(addList())}> */}
+			<button onClick={addList}>Create a new list</button>
 		</section>
 	);
 };
